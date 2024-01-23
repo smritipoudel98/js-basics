1430d6e74f35efa2472085b6bf90cfc8a69e8725
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-01-23 18:58:17 +0545

    learnaboutvariable,commentandconsole

---
+++ index.js
@@ -0,0 +1,24 @@
+//print the message
+console.log("hello worlds");
+//variables (var,let, const)
+//var(ES5))
+var nameStudent = "smriti"; // camelcase for joining two or more words for variables.
+nameStudent = "shruti"; // we can rewrite the var keyword.
+console.log(nameStudent);
+//const,let(ES6)
+let student = "shyam";
+student = "hari";
+console.log(student);
+
+const st = "girl";
+// st = "hari"; can't write st(cannot be rewritten.)(single line comment))
+console.log(st);
+
+/*question create 4 variables using const keyword for (multiline comment))
+table, fan, chair, pen.*/
+const tableName = "Baltra";
+const fanColor = "red";
+const chairMove = "moving";
+const pen = "black";
+console.log(tableName, fanColor, chairMove, pen);
+console.log(tableName + fanColor, chairMove, pen);
