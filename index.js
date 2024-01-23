81f15b9e0eb7d990b7140dc039d45fd2c89ef08d
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-01-24 00:10:33 +0545

    assignment1

--- index.js
+++ index.js
@@ -22,3 +22,28 @@ const chairMove = "moving";
 const pen = "black";
 console.log(tableName, fanColor, chairMove, pen);
 console.log(tableName + fanColor, chairMove, pen);
+//assignment1
+
+/*Firstly we use var keyword. 
+It  has hoisting where hoisting means to moved to current position. 
+var can be used if you want function-scoped variables that can be hoisted.
+Here in an example, saraswati saraswati is an output because krishna has been discarded by the current value saraswati.
+*/
+var sitaParent = "shyam";
+var sitaParent = "rita"; //can redeclare in the same scope.
+console.log(sitaParent, sitaParent); //rita rita <-output.
+
+/* let keyword.
+ let can be used if you want block-scoped variables that can be reassigned. 
+*/
+let hariParent = "saraswati";
+hariParent = "krishna";
+// let hariParent = "Saraswati"; can't redeclare in the same scope.
+console.log(hariParent + hariParent); //krishnakrishna <-output.
+
+/*const keyword.
+Use const if you want block-scoped variables that are constant and cannot be reassigned.
+*/
+const pieValue = "3.14";
+// pieValue = "Math";(pieValue can't be reassigned because it contains constants values only.)
+console.log(pieValue); //3.14 <-output.
