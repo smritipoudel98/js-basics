9d49c13b48b77fa579034ae2f4f2955052b0ae78
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-02-08 12:52:28 +0545

    filter()

--- youtube.js
+++ youtube.js
@@ -9,7 +9,16 @@
 // console.log(res);
 
 //Make an array of numbers that are doubles of the first array
-let array = [1, 2, 3, 4];
-let a = array.map((value) => Math.pow(value, 2));
+// let array = [1, 2, 3, 4];
+// let a = array.map((value) => Math.pow(value, 2));
 
-console.log(a);
+// console.log(a);
+
+//filter()
+// const a = ["ram", "shyam", 12, 34, 6];
+// const b = a.filter((value) => value > 6);
+// console.log(b);
+//reduce()
+const a = [10, 34, 6, "hari"];
+const b = a.reduce((value1, value2) => value1 + value2);
+console.log(b);
