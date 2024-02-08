8f77725797838ef0299d312ac04125b058f9980c
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-02-08 12:27:05 +0545

    power_array_map()

--- youtube.js
+++ youtube.js
@@ -1,8 +1,15 @@
-let employee = {
-  name: "smriti",
-  age: 23,
-  roll: 31,
-  "channel 2": "deees",
-};
-const res = employee["channel 2"];
-console.log(res);
+// space in object
+// let employee = {
+//   name: "smriti",
+//   age: 23,
+//   roll: 31,
+//   "channel 2": "deees",
+// };
+// const res = employee["channel 2"];
+// console.log(res);
+
+//Make an array of numbers that are doubles of the first array
+let array = [1, 2, 3, 4];
+let a = array.map((value) => Math.pow(value, 2));
+
+console.log(a);
