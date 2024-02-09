2328d63d7802382c512c54beca55c8f795895757
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-02-09 14:11:43 +0545

    emailvalidator()

--- index.js
+++ index.js
@@ -23,47 +23,30 @@
 // const input = lodash.truncate("This is an Example String", { length: 10 });
 // console.log({ input });
 
-// const QRCode = require("qrcode");
-// const canvas = document.getElementById("canvas");
-
-// QRCode.toCanvas(canvas, "sample text", function (error) {
-//   if (error) console.error(error);
-//   console.log("success!");
-// });
-
 // var QRCode = require("qrcode");
-
-// QRCode.toDataURL("I am a pony!", function (err, url) {
-//   console.log(url);
-// });
-// var QRCode = require("qrcode");
-
 // QRCode.toString("I am a pony!", { type: "terminal" }, function (err, url) {
 //   console.log(url);
 // });
 
 // const CC = require("currency-converter-lt");
+// const currencyConverter = new CC();
+// currencyConverter
+//   .from("USD")
+//   .to("GBP")
+//   .amount(125)
+//   .convert()
+//   .then((response) => {
+//     console.log(response); //or do something else
+//   });
 
-// let currencyConverter = new CC();
-
-// let ratesCacheOptions = {
-//   isRatesCaching: true, // Set this boolean to true to implement rate caching
-//   ratesCacheDuration: 3600, // Set this to a positive number to set the number of seconds you want the rates to be cached. Defaults to 3600 seconds (1 hour)
-// };
-
-// currencyConverter = currencyConverter.setupRatesCache(ratesCacheOptions);
-
-const CC = require("currency-converter-lt");
-const currencyConverter = new CC();
-currencyConverter
-  .from("USD")
-  .to("GBP")
-  .amount(125)
-  .convert()
-  .then((response) => {
-    console.log(response); //or do something else
-  });
-// let currencyConverter = new CC({ from: "USD", to: "JPY", amount: 100 });
-
+//dividendConverter
+// const floor = (a) => Math.floor(a);
 // const _ = require("lodash");
-// _.floor(4.006);
+// const ress = _.floor(4.006);
+// console.log(ress);
+
+//emailvalidator
+const validator = require("email-validator");
+
+const a = validator.validate("test@email.com"); // true
+console.log(a);
