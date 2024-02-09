70b5edc864009d6180222281eb73216c6e9b6156
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-02-09 18:25:22 +0545

    email

--- index.js
+++ index.js
@@ -46,7 +46,83 @@
 // console.log(ress);
 
 //emailvalidator
-const validator = require("email-validator");
+// const validator = require("email-validator");
 
-const a = validator.validate("test@email.com"); // true
-console.log(a);
+// const a = validator.validate("test@email.com"); // true
+// console.log(a);
+
+//generate pdf from text and image
+// const fs = require("fs");
+// const PDFDocument = require("pdfkit");
+
+// // Create a new PDF document
+// const doc = new PDFDocument();
+
+// // Pipe the PDF output to a file
+// const outputStream = fs.createWriteStream("output.pdf");
+// doc.pipe(outputStream);
+
+// // Add text to the PDF
+// doc.fontSize(24).text("Example PDF with Text and Images", { align: "center" });
+
+// // Add an image to the PDF
+// // doc.image("E:photosIMG_20211015_120024.jpg", { width: 400, align: "center" });
+
+// // Add more text
+// // doc
+// //   .fontSize(16)
+// //   .text("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", {
+// //     align: "justify",
+// //     lineGap: 10,
+// //   });
+
+// // // Finalize the PDF
+// // doc.end();
+
+// // // Log a message when the PDF generation is complete
+// // outputStream.on("finish", () => {
+// //   console.log("PDF generated successfully.");
+// // });
+
+// const axios = require("axios");
+
+// // OpenWeatherMap API URL and API key
+// const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+// const API_KEY = "c6c643f3d8cf1a5de41efc636bb18524";
+
+// // // Function to fetch weather data
+// async function fetchWeather(cityName) {
+//   try {
+//     const response = await axios.get(API_URL, {
+//       params: {
+//         q: cityName,
+//         appid: API_KEY,
+//         units: "metric", // You can change units to 'imperial' for Fahrenheit
+//       },
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching weather data:", error);
+//     throw error;
+//   }
+// }
+
+// // Example usage
+// const cityName = "London"; // Example city name
+// fetchWeather(cityName)
+//   .then((data) => {
+//     console.log("Weather data:", data);
+//     // Handle weather data here
+//   })
+//   .catch((error) => {
+//     // Handle error
+//   });
+
+const { mailer } = require("./practice");
+const sendMail = async (email, sub) => {
+  return mailer(email, sub);
+};
+sendMail("ishupoudel27@gmail.com", "hi ishu").then((res) => {
+  console.log(res);
+});
--- practice.js
+++ practice.js
@@ -1,4 +1,42 @@
-const truncate = (g) => {
-  return g.slice(0, 2) + "...";
+// const truncate = (g) => {
+//   return g.slice(0, 2) + "...";
+// };
+// module.exports = { truncate };
+
+//write a node module to send an email(nodemailer)
+//write a node module to encrypt the password(bcryptjs)
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: "",
+    pass: "",
+  },
+});
+
+// // async..await is not allowed in global scope, must use a wrapper
+const mailer = async (email, subject) => {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Ishu poudel" <foo@example.com>', // sender address
+    to: email, // list of receivers
+    subject: subject, // Subject line
+    html: "<b>Hello world?</b>", // html body
+  });
+  return info.messageId; //
 };
-module.exports = { truncate };
+
+module.exports = { mailer };
+
+//module to encrypt password
+// var bcrypt = require("bcryptjs");
+// bcrypt.genSalt(10, function (err, salt) {
+//   bcrypt.hash("B4c0//", salt, function (err, hash) {
+//     // Store hash in your password DB.
+//   });
+// });
